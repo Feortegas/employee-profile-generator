c83d5685c6f73e4835159bd2977ff3c830f1a2fe
Fernando Ortega <feortegas@gmail.com>
2021-12-19 21:43:41 -0600

    refactor questions using async await when creating new employee

--- index.js
+++ index.js
@@ -1,21 +1,93 @@
-const questions = require('./src/questions');
-const Employee = require('./lib/Employee');
+// const questions = require('./src/questions');
+const Questions = require('./src/questions');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const { async } = require('rxjs');
+const inquirer = require('inquirer');
 
 
-function init() {
-    console.log('Welcome to the Employee Management System at Tech.com');
-    newEmployee();
+// New Manager function
+newManager = async () => {
+    const manager = new Manager();
+    manager.name = await manager.getName();
+    manager.id = await manager.getId();
+    manager.email = await manager.getEmail();
+    manager.officeNumber = await manager.getOfficeNumber();
+    manager.role = 'Manager';
+    
+    return await choseNewEmployee();
 };
 
-const newEmployee = function() {
-    questions.promptUser()
-        .then(employeeData => {
-            return employeeData;
-        });
+const choseNewEmployee = async () => {
+    const getRole = await inquirer.prompt({
+        // inquirer - ask for employee role - Engineer or Intern
+        type: 'list',
+        name: 'role',
+        message: `Chose to create an Engineer or an Intern or exit the app`,
+        choices: ['Engineer', 'Intern', 'exit app']
+    })
+    const role = await getRole.role;
     
-    
-    console.log(newEmployee);
+    if (role === 'Engineer') {
+        await newEngineer();
+    } else if (role === 'Intern') {
+        await newIntern();
+    } else if (role === 'exit app') {
+        await createPage();
+    }
+};
 
+// New Engineer function
+const newEngineer = async () => {
+    const engineer = new Engineer();
+    engineer.name = await engineer.getName();
+    engineer.id = await engineer.getId();
+    engineer.email = await engineer.getEmail();
+    engineer.gitHub = await engineer.getGitHub();
+    engineer.role = 'Engineer';
+
+    return await choseNewEmployee();
+};
+
+
+// New Intern function
+const newIntern = async () => {
+    const intern = new Intern();
+    intern.name = await intern.getName();
+    intern.id = await intern.getId();
+    intern.email = await intern.getEmail();
+    intern.school = await intern.getSchool();
+    intern.role = 'Intern';
+
+    return await choseNewEmployee();
+};
+
+// Create HTML file
+const createPage = async () => {
+    console.log('create page');
+};
+
+function init() {
+    console.log('Welcome to Tech.com Employee management app.');
+
+    // create new manager
+    newManager();
+   
+
+    // create Manager Employee
+    // const nextEmployee = newManager();
+
+    // // check what employee role to create next
+    // if (employeeData.role === 'Engineer') {
+    //     // prompt Engineer questions
+    //     return new Questions().promptUserEngineer();
+    // } else if (employeeData.role === 'Intern') {
+    //     // prompt Intern questions
+    //     return new Questions().promptUserIntern();
+    // } else if (employeeData.role === 'exit app') {
+    //     return employeeData;
+    // }
 }
 
 init();
--- src/questions.js
+++ src/questions.js
@@ -1,40 +1,219 @@
 const inquirer = require('inquirer');
 
-const promptUser = employeeData => {
-    return inquirer.prompt([
-        {
-            // inquirer - ask for employee's name
-            type: 'text',
-            name: 'name',
-            message: `Enter the Employee's name:`
-        },
-        {
-            // inquirer - ask for employee id
-            type: 'text',
-            name: 'id',
-            message: `Enter the Employee's id:`
-        },
-        {
-            // inquirer - ask for employee email
-            type: 'text',
-            name: 'email',
-            message: `Enter the Employee's email:`
-        },
-        {
-            // inquirer - ask for employee office number
-            type: 'text',
-            name: 'officeNumber',
-            message: `Enter the Employee's office number:`
-        }
-    ]);
-};
+class Questions {
+    constructor() {
 
-module.exports = { promptUser };
+    };
 
-// {
-//     // inquirer - ask for employee role - Engineer or Intern
-//     type: 'text',
-//     name: 'name',
-//     message: `Enter the Employee's office number:`,
-//     choices: ['Engineer', 'Intern']
-// }
+    promptUserManager(employeeData) {
+        return inquirer.prompt([
+            {
+                // inquirer - ask for employee's name
+                type: 'input',
+                name: 'name',
+                message: `Enter the Employee's name:`
+            },
+            {
+                // inquirer - ask for employee id
+                type: 'input',
+                name: 'id',
+                message: `Enter the Employee's id:`
+            },
+            {
+                // inquirer - ask for employee email
+                type: 'input',
+                name: 'email',
+                message: `Enter the Employee's email:`
+            },
+            {
+                // inquirer - ask for employee office number
+                type: 'input',
+                name: 'officeNumber',
+                message: `Enter the Employee's office number:`
+            },
+            {
+                // inquirer - ask for employee role - Engineer or Intern
+                type: 'list',
+                name: 'role',
+                message: `Chose to create an Engineer or an Intern or exit the app`,
+                choices: ['Engineer', 'Intern', 'exit app']
+            }
+        ]);
+    }
+
+    promptUserEngineer(employeeData) {
+        return inquirer.prompt([
+            {
+                // inquirer - ask for employee's name
+                type: 'input',
+                name: 'name',
+                message: `Enter the Employee's name:`
+            },
+            {
+                // inquirer - ask for employee id
+                type: 'input',
+                name: 'id',
+                message: `Enter the Employee's id:`
+            },
+            {
+                // inquirer - ask for employee email
+                type: 'input',
+                name: 'email',
+                message: `Enter the Employee's email:`
+            },
+            {
+                // inquirer - ask for employee GitHub User Name
+                type: 'input',
+                name: 'github',
+                message: `Enter the Employee's GitHub User Name:`
+            },
+            {
+                // inquirer - ask for employee role - Engineer or Intern
+                type: 'list',
+                name: 'role',
+                message: `Chose to create an Engineer or an Intern or exit the app`,
+                choices: ['Engineer', 'Intern', 'exit app']
+            }
+        ]);
+    };
+
+    promptUserIntern(employeeData) {
+        return inquirer.prompt([
+            {
+                // inquirer - ask for employee's name
+                type: 'input',
+                name: 'name',
+                message: `Enter the Employee's name:`
+            },
+            {
+                // inquirer - ask for employee id
+                type: 'input',
+                name: 'id',
+                message: `Enter the Employee's id:`
+            },
+            {
+                // inquirer - ask for employee email
+                type: 'input',
+                name: 'email',
+                message: `Enter the Employee's email:`
+            },
+            {
+                // inquirer - ask for employee School
+                type: 'input',
+                name: 'school',
+                message: `Enter the Employee's School:`
+            },
+            {
+                // inquirer - ask for employee role - Engineer or Intern
+                type: 'list',
+                name: 'role',
+                message: `Chose to create an Engineer or an Intern or exit the app`,
+                choices: ['Engineer', 'Intern', 'exit app']
+            }
+        ]);
+    };
+
+
+}
+
+
+
+// const promptUserManager = employeeData => {
+//     return inquirer.prompt([
+//         {
+//             // inquirer - ask for employee's name
+//             type: 'input',
+//             name: 'name',
+//             message: `Enter the Employee's name:`
+//         },
+//         {
+//             // inquirer - ask for employee id
+//             type: 'input',
+//             name: 'id',
+//             message: `Enter the Employee's id:`
+//         },
+//         {
+//             // inquirer - ask for employee email
+//             type: 'input',
+//             name: 'email',
+//             message: `Enter the Employee's email:`
+//         },
+//         {
+//             // inquirer - ask for employee office number
+//             type: 'input',
+//             name: 'officeNumber',
+//             message: `Enter the Employee's office number:`
+//         },
+//         {
+//             // inquirer - ask for employee role - Engineer or Intern
+//             type: 'list',
+//             name: 'role',
+//             message: `Chose to create an Engineer or an Intern or exit the app`,
+//             choices: ['Engineer', 'Intern', 'exit app']
+//         }
+//     ]);
+// };
+
+// const promptUserEngineer = employeeData => {
+//     return inquirer.prompt([
+//         {
+//             // inquirer - ask for employee's name
+//             type: 'input',
+//             name: 'name',
+//             message: `Enter the Employee's name:`
+//         },
+//         {
+//             // inquirer - ask for employee id
+//             type: 'input',
+//             name: 'id',
+//             message: `Enter the Employee's id:`
+//         },
+//         {
+//             // inquirer - ask for employee email
+//             type: 'input',
+//             name: 'email',
+//             message: `Enter the Employee's email:`
+//         },
+//         {
+//             // inquirer - ask for employee GitHub User Name
+//             type: 'input',
+//             name: 'github',
+//             message: `Enter the Employee's GitHub User Name:`
+//         }
+//     ]);
+// };
+
+// const promptUserIntern = employeeData => {
+//     return inquirer.prompt([
+//         {
+//             // inquirer - ask for employee's name
+//             type: 'input',
+//             name: 'name',
+//             message: `Enter the Employee's name:`
+//         },
+//         {
+//             // inquirer - ask for employee id
+//             type: 'input',
+//             name: 'id',
+//             message: `Enter the Employee's id:`
+//         },
+//         {
+//             // inquirer - ask for employee email
+//             type: 'input',
+//             name: 'email',
+//             message: `Enter the Employee's email:`
+//         },
+//         {
+//             // inquirer - ask for employee School
+//             type: 'input',
+//             name: 'school',
+//             message: `Enter the Employee's School:`
+//         }
+//     ]);
+// };
+
+// module.exports = { promptUserEngineer };
+// module.exports = { promptUserIntern };
+// module.exports = { promptUserManager };
+
+module.exports = Questions;
